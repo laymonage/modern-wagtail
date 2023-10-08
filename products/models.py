cadeb2132dabc34c26522ec3cb630ee1db58d56a
Sage Abdullah <me@laymonage.com>
2023-10-08 22:40:37 +0100

    Add Product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    discount = models.PositiveSmallIntegerField()
+    stock = models.PositiveIntegerField()
+    image_url = models.URLField(
+        max_length=2047,
+        blank=True,
+        default="https://picsum.photos/256",
+    )
+
+    @property
+    def final_price(self):
+        return self.price * (1 - self.discount / 100)
