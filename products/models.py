ed6a27e56b25b5069946bbbefab60d9f70dfd98f
Sage Abdullah <me@laymonage.com>
2023-10-14 05:43:02 -0700

    Enable revisions

--- products/models.py
+++ products/models.py
@@ -2,12 +2,12 @@ from decimal import Decimal
 from django.db import models
 
 from wagtail.admin.panels import FieldPanel
-from wagtail.models import PreviewableMixin
+from wagtail.models import PreviewableMixin, RevisionMixin
 from wagtail.snippets.models import register_snippet
 
 
 @register_snippet
-class Product(PreviewableMixin, models.Model):
+class Product(RevisionMixin, PreviewableMixin, models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
